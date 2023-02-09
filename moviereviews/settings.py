a5c45a4d0474825b75a3149f93ac24739e57656d
jagomezd03 <jagomezd@eafit.edu.co>
2023-02-09 12:00:33 -0500

    taller 2

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 import os
 
-MEDIA_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about', movieviews.about),
+    path('about/', movieviews.about),
     path('', movieviews.home),
 ]
 
