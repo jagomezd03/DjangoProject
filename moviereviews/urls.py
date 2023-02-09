b1ef4252982bbdb21f3230f9356a123851211389
jagomezd03 <jagomezd@eafit.edu.co>
2023-02-09 13:35:54 -0500

    correccion de errores

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -19,10 +19,11 @@ from movie import views as movieviews
 
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', movieviews.about),
     path('', movieviews.home),
+    path('about/', movieviews.about),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
