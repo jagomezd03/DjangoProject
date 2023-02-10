f545b7beaf847988258ac46a8dd454cedd39ee9b
jagomezd03 <jagomezd@eafit.edu.co>
2023-02-10 11:17:36 -0500

    correcciones

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.1
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
