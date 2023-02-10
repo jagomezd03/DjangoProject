10414ce2c0577f5f871e0d5edfcaced3b692ad44
jagomezd03 <jagomezd@eafit.edu.co>
2023-02-10 15:27:42 -0500

    traduccion formulario de correo e identacion de codigo

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request,'home.html')
-    return render(request, 'home.html', {'name':'Juan Andres Gomez'})
+    #return render(request, 'home.html', {'name':'Juan Andres Gomez'})
+    searchTerm = request.GET.get('searchmovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request,'about.html')
     return render(request, 'about.html', {'name':'Juan Andres Gomez'})
