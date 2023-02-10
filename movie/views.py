63d79a5b7d0666f9b18b8dedf82ac0bf65ec71ab
jagomezd03 <jagomezd@eafit.edu.co>
2023-02-10 15:49:14 -0500

    buscador actualizado

--- movie/views.py
+++ movie/views.py
@@ -10,8 +10,11 @@ def home(request):
     #return render(request,'home.html')
     #return render(request, 'home.html', {'name':'Juan Andres Gomez'})
     searchTerm = request.GET.get('searchmovie')
-    movies = Movie.objects.all()
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
-    return render(request, 'about.html', {'name':'Juan Andres Gomez'})
+    return render(request, 'about.html')
